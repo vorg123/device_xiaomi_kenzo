149a9b1f6d199aa62a62b95f32d985211ffdf654
Kyryll Bondarenko <rolfpeterkesche@googlemail.com>
2019-11-07 06:11:16 +0200

    Havocify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,8 +14,8 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_kenzo.mk
+    $(LOCAL_DIR)/havoc_kenzo.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_kenzo-userdebug \
-    lineage_kenzo-eng
+    havoc_kenzo-userdebug \
+    havoc_kenzo-eng
