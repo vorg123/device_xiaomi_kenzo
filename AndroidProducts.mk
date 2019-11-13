9cf8bd6d347615523e7d1eae9423f37659f83b76
Kyryll Bondarenko <rolfpeterkesche@googlemail.com>
2019-11-13 18:16:29 +0200

    bootleggers

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,8 +14,8 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/havoc_kenzo.mk
+    $(LOCAL_DIR)/bootleg_kenzo.mk
 
 COMMON_LUNCH_CHOICES := \
-    havoc_kenzo-userdebug \
-    havoc_kenzo-eng
+    bootleg_kenzo-userdebug \
+    bootleg_kenzo-eng
